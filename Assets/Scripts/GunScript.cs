b6f2376b06c7e8b9ec685e331ca16b61a94944ea
TheRedKraken <theredkrakenexists@gmail.com>
2019-01-26 19:11:37 +0000

    Removed cam shake

--- Assets/Scripts/GunScript.cs
+++ Assets/Scripts/GunScript.cs
@@ -14,11 +14,6 @@ public class GunScript : MonoBehaviour
     float timeToSpawnEffect = 0;
     public float effectSpawnRate = 10;
 
-    // Handle camera shaking
-    public float camShakeAmt = 0.05f;
-    public float camShakeLength = 0.1f;
-    CameraShake camShake;
-
     float timeToFire = 0;
     Transform firePoint;
 
@@ -34,9 +29,6 @@ public class GunScript : MonoBehaviour
 
     void Start()
     {
-        camShake = GameMaster.gm.GetComponent<CameraShake>();
-        if (camShake == null)
-            Debug.LogError("No CameraShake script found on GM object.");
     }
 
     // Update is called once per frame
@@ -97,7 +89,6 @@ public class GunScript : MonoBehaviour
             Effect(hitPos, hitNormal);
             timeToSpawnEffect = Time.time + 1 / effectSpawnRate;
             */
-            camShake.Shake(camShakeAmt, camShakeLength);
         }
     }
 
@@ -125,8 +116,5 @@ public class GunScript : MonoBehaviour
         float size = Random.Range(0.6f, 0.9f);
         clone.localScale = new Vector3(size, size, size);
         Destroy(clone.gameObject, 0.02f);
-
-        //Shake the camera
-        camShake.Shake(camShakeAmt, camShakeLength);
     }
 }
