4ccdd3e8bfc8cf77615f27f16cf3f5878e28db65
mrpatinhazz <jguilhermefgomes@gmail.com>
2019-01-26 21:27:28 +0000

    Tree Changes

---
+++ Assets/Scripts/AxeScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AxeScript : MonoBehaviour
+{
+    public GameObject tree;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Tree")
+        {
+            Debug.Log("collision.gameObject.tag");
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        Debug.Log("Can cut");
+        if(Input.GetMouseButton(0) || Input.GetMouseButton(1))
+        {
+            Debug.Log("Cutting");
+        }
+    }
+
+}
---
+++ Assets/Scripts/TreeController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeController : MonoBehaviour
+{
+    [System.Serializable]
+    public class TreeStats
+    {
+        public int Health = 100;
+    }
+
+    public TreeStats stats = new TreeStats();
+
+    public void DamageTree(int damage)
+    {
+        stats.Health -= damage;
+        if (stats.Health <= 0)
+        {
+            GameMaster.CutTree(this);
+        }
+    }
+}
