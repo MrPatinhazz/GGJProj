3fccf240aeb1c6105450dc7bf3852c2f954dc301
mrpatinhazz <jguilhermefgomes@gmail.com>
2019-01-26 15:43:20 +0000

    Wave Spawner fixed position

---
+++ Assets/VisualSpawnArea.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VisualSpawnArea : MonoBehaviour
+{
+    private Vector3 origin;
+    private Vector3 range;
+    private Vector3 randomRange;
+    private Vector3 randomCoordinate;
+
+    public void Start()
+    {
+        Vector3 origin = transform.position;
+        Vector3 range = transform.localScale / 2.0f;
+    }
+
+    public void Update()
+    {
+        Vector3 randomRange = new Vector3(Random.Range(-range.x, range.x),
+                                          Random.Range(-range.y, range.y),
+                                          Random.Range(-range.z, range.z));
+        Vector3 randomCoordinate = randomRange;
+        print(randomRange);
+    }
+
+    public Color GizmosColor = new Color(0.5f, 0.5f, 0.5f, 0.2f);
+    public Color DotColor = new Color(0.1f, 0.1f, 0.1f, 1f);
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = GizmosColor;
+        Gizmos.DrawCube(transform.position, transform.localScale);
+        Gizmos.color = DotColor;
+        Gizmos.DrawWireSphere(randomCoordinate, 1f);
+    }
+}
---
+++ Assets/WaveSpawner.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveSpawner : MonoBehaviour
+{
+    public enum SpawnState { SPAWNING, WAITING, COUNTING};
+
+    [System.Serializable]
+    public class Wave
+    {
+        public string name;
+        public Transform enemy;
+        public int count;
+        public float rate;
+    }
+
+    public Wave[] waves;
+    private int _nextWave = 0;
+
+    public float timeBetweenWaves = 5f;
+    public float waveCountdown;
+
+    private float _searchCtd = 1f;
+
+    private SpawnState state = SpawnState.COUNTING;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        waveCountdown = timeBetweenWaves;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (state == SpawnState.WAITING)
+        {
+            if (!EnemyIsAlive())
+            {
+                WaveCompleted();
+            }
+            else
+            {
+                return;
+            }
+        }
+
+        if(waveCountdown <= 0)
+        {
+            if(state != SpawnState.SPAWNING)
+            {
+                StartCoroutine(SpawnWave(waves[_nextWave]));
+            }
+
+        }
+        else
+        {
+            waveCountdown -= Time.deltaTime;
+        }
+    }
+
+    bool EnemyIsAlive()
+    {
+        _searchCtd -= Time.deltaTime;
+        if(_searchCtd <= 0f)
+        {
+            _searchCtd = 1f;
+            if (GameObject.FindGameObjectWithTag("Enemy") == null)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    IEnumerator SpawnWave(Wave _wave)
+    {
+        Debug.Log("Spawn wave" + _wave.name);
+        state = SpawnState.SPAWNING;
+
+        for(int i = 0; i < _wave.count; i++)
+        {
+            SpawnEnemy(_wave.enemy);
+            yield return new WaitForSeconds(1f / _wave.rate);
+        }
+
+        state = SpawnState.WAITING;
+
+        yield break;
+    }
+
+    void WaveCompleted()
+    {
+        state = SpawnState.COUNTING;
+        waveCountdown = timeBetweenWaves;
+        
+        if(_nextWave + 1 > waves.Length -1)
+        {
+            _nextWave = 0;
+            //can finish here
+            Debug.Log("All waves completed");
+        }
+
+        _nextWave++;
+    }
+
+    void SpawnEnemy (Transform _enemy)
+    {
+        Instantiate(_enemy, transform.position, transform.rotation);
+        Debug.Log("spawn enemy" + _enemy.name);
+    }
+}
