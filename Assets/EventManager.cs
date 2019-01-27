a0e62a168ef8b6a557d6f85bf706e4575bb4ba81
mrpatinhazz <jguilhermefgomes@gmail.com>
2019-01-27 02:35:43 +0000

    Event manager start

---
+++ Assets/EventManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventManager : MonoBehaviour
+{
+    [System.Serializable]
+    public class Event
+    {
+        public string text;
+        public int momHp;
+        public int kidHp;
+        public bool dogAlive;
+        public int upkeepCost;
+        public int leaveCost;
+        public int gunRate;
+        public int gunDmg;
+        public int axeRate;
+        public int axeDmg;
+        public int food;
+        public int wood;
+    }
+
+    public Event[] events;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+}
